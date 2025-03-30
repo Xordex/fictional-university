0d34097c4473243a9a78078071a0b6e83647e7a3
MichałLapek <azjaroch@wp.pl>
2025-03-30 19:57:03 +0200

    Add template parts

--- single-program.php
+++ single-program.php
@@ -5,16 +5,8 @@
     while(have_posts()) {
         the_post();
         $eventDate = new DateTime(get_field('event_date'));
+        pageBanner();
         ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Dont forget to replace me later</p>
-        </div>
-      </div>
-    </div>
 
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
@@ -62,7 +54,7 @@
     
             <li class="professor-card__list-item">
                  <a href="<?php the_permalink(); ?>" class="professor-card">
-                    <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape');?>" alt="">
+                    <img class="professor-card__image" src="<?php the_post_thumbnail_url();?>" alt="">
                     <span class="professor-card__name"><?php the_title(); ?></span>
                  </a>
             </li>
@@ -98,23 +90,9 @@
 
             while($relatedPosts-> have_posts()) {
               $relatedPosts-> the_post();
-            
-          ?>
 
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="<?php echo get_permalink();?>">
-              <span class="event-summary__month"><?php
-              $eventDate =  new DateTime(get_field('event_date'));
-              echo $eventDate->format('M');              
-              ?></span>
-              <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo get_permalink();?>"><?php the_title();?></a></h5>
-              <p><?php if(has_excerpt()) {echo get_the_excerpt();} else {echo wp_trim_words(get_the_content(), 12);} ?> <a href="<?php echo get_permalink();?>" class="nu gray">Learn more</a></p>
-            </div>
-            </div>
-            <?php
+          get_template_part('template-parts/content-event');
+            
                 }
             }
             ?>
