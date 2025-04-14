e715116b14ecdc11e4ea67f01b95073a23b484ae
Michal <kontakt@michaljaroch.pl>
2025-04-14 21:43:00 +0200

    Separate Search Results depends on post type

--- build/index.js
+++ build/index.js
@@ -4068,42 +4068,90 @@ class Search {
     }
     this.previousValue = this.inputSearch.value;
   }
-  async getResults() {
-    try {
-      const endpoints = ["posts", "pages", "event", "professor", "program", "campus"];
-      let results = [];
-      for (const endpoint of endpoints) {
-        let response = await fetch(`${universityData.root_url}/wp-json/wp/v2/${endpoint}?search=${this.inputSearch.value}`);
-        const resultsjson = await response.json();
-        results = results.concat(resultsjson);
-      }
-
-      // const posts = await fetch(universityData.root_url + `/wp-json/wp/v2/posts?search=${this.inputSearch.value}`);
-      // const postsjson = await posts.json();
-      // const pages = await fetch(universityData.root_url + `/wp-json/wp/v2/pages?search=${this.inputSearch.value}`);
-      // const pagesjson = await pages.json();
-      // const event = await fetch(universityData.root_url + `/wp-json/wp/v2/event?search=${this.inputSearch.value}`);
-      // const eventjason = await event.json();
-      // const professor = await fetch(universityData.root_url + `/wp-json/wp/v2/professor?search=${this.inputSearch.value}`);
-      // const professorjason = await professor.json();
-      // const program = await fetch(universityData.root_url + `/wp-json/wp/v2/program?search=${this.inputSearch.value}`);
-      // const programjson = await program.json();
-      // const campus = await fetch(universityData.root_url + `/wp-json/wp/v2/campus?search=${this.inputSearch.value}`);
-      // const campusejson = await campus.json();
-
-      // let results = postsjson.concat(pagesjson).concat(eventjason).concat(professorjason).concat(programjson).concat(campusejson);
+  getResults() {
+    fetch(`${universityData.root_url}/wp-json/university/v1/search?term=${this.inputSearch.value}`).then(response => response.json()).then(data => {
+      console.log(data);
       this.resultsDiv.innerHTML = `
-                    <h2 class="search-overlay__section-title">Search Results</h2>
-                    ${results.length > 0 ? `
+            <div class="row">
+                <div class="one-third">
+                    <h2 class="search-overlay__section-title">General Information</h2>
+                    ${data.generalInfo.length > 0 ? `
+                    <ul class="link-list min-list">
+                    ${data.generalInfo.map(e => `<li><a href="${e.permalink}">${e.title}</a> ${e.postType == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
+                    </ul>
+                    ` : `<p>Brak wpisów dla tego zapytania. <a href="${universityData.root_url}/blog">Zobacz wszystkie wpisy</a></p>`}
+                        
+                </div>
+                <div class="one-third">
+                    <h2 class="search-overlay__section-title">Programs</h2>
+                    ${data.programs.length > 0 ? `
+                    <ul class="link-list min-list">
+                    ${data.programs.map(e => `<li><a href="${e.permalink}">${e.title}</a></li>`).join("")}
+                    </ul>
+                    ` : `<p>Brak programów dla tego zapytania. <a href="${universityData.root_url}/programs">Zobacz wszystkie programy</a></p>`}  
+
+                    <h2 class="search-overlay__section-title">Professors</h2>
+                    ${data.professors.length > 0 ? `
                         <ul class="link-list min-list">
-                        ${results.map(e => `<li><a href="${e.link}">${e.title.rendered}</a> ${e.type == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
+                        ${data.professors.map(e => `<li><a href="${e.permalink}">${e.title}</a> ${e.postType == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
                         </ul>
-                        ` : "<p>Brak wyników dla tego zapytania.</p>"}
-                        `;
-      this.isSpinnerVisible = false;
-    } catch (error) {
-      this.resultsDiv.innerHTML = error.message;
-    }
+                        ` : `<p>Brak profesorów dla tego zapytania. <a href="${universityData.root_url}/professors">Zobacz wszystkich profesorów</a></p>`}  
+                </div>
+                <div class="one-third">
+                    <h2 class="search-overlay__section-title">Campuses</h2>
+                    ${data.campuses.length > 0 ? `
+                        <ul class="link-list min-list">
+                        ${data.campuses.map(e => `<li><a href="${e.permalink}">${e.title}</a> ${e.postType == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
+                        </ul>
+                        ` : `<p>Brak kampusów dla tego zapytania. <a href="${universityData.root_url}/campuses">Zobacz wszystkie kampusy</a></p>`}  
+
+                    <h2 class="search-overlay__section-title">Events</h2>
+                    ${data.events.length > 0 ? `
+                        <ul class="link-list min-list">
+                        ${data.events.map(e => `<li><a href="${e.permalink}">${e.title}</a> ${e.postType == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
+                        </ul>
+                        ` : `<p>Brak wydarzeń dla tego zapytania. <a href="${universityData.root_url}/events">Zobacz wszystkie wydarzenia</a></p>`}   
+                </div>
+            </div>`;
+    });
+    this.isSpinnerVisible = false;
+
+    // try {
+    //     const endpoints = ["posts", "pages", "event", "professor", "program", "campus"];
+    //     let results = [];
+    //     for (const endpoint of endpoints) {
+    //         let response = await fetch(`${universityData.root_url}/wp-json/wp/v2/${endpoint}?search=${this.inputSearch.value}`);
+    //         const resultsjson = await response.json();
+    //         results = results.concat(resultsjson);
+    //     }
+
+    //     // const posts = await fetch(universityData.root_url + `/wp-json/wp/v2/posts?search=${this.inputSearch.value}`);
+    //     // const postsjson = await posts.json();
+    //     // const pages = await fetch(universityData.root_url + `/wp-json/wp/v2/pages?search=${this.inputSearch.value}`);
+    //     // const pagesjson = await pages.json();
+    //     // const event = await fetch(universityData.root_url + `/wp-json/wp/v2/event?search=${this.inputSearch.value}`);
+    //     // const eventjason = await event.json();
+    //     // const professor = await fetch(universityData.root_url + `/wp-json/wp/v2/professor?search=${this.inputSearch.value}`);
+    //     // const professorjason = await professor.json();
+    //     // const program = await fetch(universityData.root_url + `/wp-json/wp/v2/program?search=${this.inputSearch.value}`);
+    //     // const programjson = await program.json();
+    //     // const campus = await fetch(universityData.root_url + `/wp-json/wp/v2/campus?search=${this.inputSearch.value}`);
+    //     // const campusejson = await campus.json();
+
+    //     // let results = postsjson.concat(pagesjson).concat(eventjason).concat(professorjason).concat(programjson).concat(campusejson);
+    // //     this.resultsDiv.innerHTML = `
+    // //             <h2 class="search-overlay__section-title">Search Results</h2>
+    // //             ${results.length > 0 ? `
+    // //                 <ul class="link-list min-list">
+    // //                 ${results.map(e => `<li><a href="${e.link}">${e.title.rendered}</a> ${e.type == 'post' ? `by ${e.authorName}</li>` : ''}`).join("")}
+    // //                 </ul>
+    // //                 ` : "<p>Brak wyników dla tego zapytania.</p>"}
+    // //                 `;
+
+    // //     this.isSpinnerVisible = false;
+    // // } catch (error) {
+    // //     this.resultsDiv.innerHTML = error.message;
+    // // }
   }
   keyPressDispatcher(ClickedBtn) {
     if (ClickedBtn.key == "Escape" && this.isOverlayOpen) this.closeOverlay();
